88a50745cafd7363e50a4ba892b79f76b09568f7
KangDaHeee <113006338+KangDaHeee@users.noreply.github.com>
2025-03-27 23:38:12 +0900

    [Bronze II] Title: 공 바꾸기, Time: 200 ms, Memory: 18524 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = i + 1;
+        }
+
+        for(int t = 0; t<m; t++){
+
+            int i = sc.nextInt() - 1;
+            int j = sc.nextInt() - 1;
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        for(int a = 0; a < n; a++){
+            System.out.print(arr[a] + " ");
+        }
+
+        sc.close();
+    }
+}
