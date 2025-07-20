28e06cf95d8e03f675247367b435774449efd8ed
KangDaHeee <113006338+KangDaHeee@users.noreply.github.com>
2025-07-20 13:16:35 +0900

    [Silver V] Title: 색종이, Time: 200 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,34 @@
+import java.util.*;
+//import java.io.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int[][] paper = new int[100][100];
+        int count = sc.nextInt();
+        int result = 0;
+
+        for(int i = 0; i < count; i++){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            for(int j = x; j < x + 10; j++){
+                for(int k = y; k < y + 10; k++){
+                    paper[k][j] = 1;
+                }
+            }
+        }
+
+        for(int i = 0; i < 100; i++){
+            for(int j = 0; j < 100; j++){
+                if(paper[i][j] == 1) {
+                    result += paper[i][j];
+                }
+            }
+        }
+
+        System.out.print(result);
+
+        sc.close();
+    }
+}
