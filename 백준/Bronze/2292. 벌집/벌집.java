4a95f392288da556c87e3293de9abb7336bde18e
KangDaHeee <113006338+KangDaHeee@users.noreply.github.com>
2025-07-21 21:23:28 +0900

    [Bronze II] Title: 벌집, Time: 180 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,21 @@
+import java.util.*;
+//import java.io.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int layer = 1;
+        int maxNum = 1;
+
+        while(n > maxNum){
+            maxNum += 6 * layer;
+            layer++;
+        }
+
+        System.out.print(layer);
+
+        sc.close();
+    }
+}
