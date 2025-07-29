a822c07c5f571551e0f54c3aa987236fed2e4b28
KangDaHeee <113006338+KangDaHeee@users.noreply.github.com>
2025-07-29 23:46:14 +0900

    [Bronze I] Title: 소인수분해, Time: 192 ms, Memory: 17832 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,21 @@
+//import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        for(int i = 2; i <= num; i++){
+            if(num == 1){
+                continue;
+            }
+            while (num % i == 0){
+                System.out.println(i);
+                num /= i;
+            }
+        }
+        sc.close();
+    }
+}
